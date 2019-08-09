8d81b2398263fbc6041cd5a961bbc5f4187d3c80
Jardel Kasai <jardel22@hotmail.co.uk>
2019-08-09 02:05:18 +0100

    Fixed database, added word templates, etc.

--- routes/web.php
+++ routes/web.php
@@ -30,6 +30,21 @@ Route::get('/home', 'HomeController@index')->name('home');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index');
-Route::get('/admin', 'AdminController@index');
-Route::get('/clerk', 'AdminController@index');
+Route::get('/users/logout', 'Auth\LoginController@userLogout')->name('user.logout');
+
+Route::prefix('admin')->group(function(){
+    Route::get('/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
+    Route::post('/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
+    Route::get('/', 'AdminController@index')->name('admin.dashboard');
+    Route::get('/logout', 'Auth\AdminLoginController@logout')->name('admin.logout');
+});
+
+Route::prefix('clerk')->group(function(){
+    Route::get('/login', 'Auth\ClerkLoginController@showLoginForm')->name('clerk.login');
+    Route::post('/login', 'Auth\ClerkLoginController@login')->name('clerk.login.submit');
+    Route::get('/', 'ClerkController@index')->name('clerk.dashboard');
+    Route::get('/logout', 'Auth\ClerkLoginController@clerkLogout')->name('clerk.logout');
+});
+
+
 
